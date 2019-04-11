33cd591ad2aed4c96f72965d52bbd1c7e42b110f
Martin Gulliksson <martin@gullik.cc>
2019-04-11 22:35:31 +0200

    added hud indicators

--- Player.gd
+++ Player.gd
@@ -11,6 +11,9 @@ const k_p = 0.009
 const k_i = 0
 const k_d = 0
 
+onready var node_hud_blue = get_node("/root/Scene/HUD/BlueIndicator")
+onready var node_hud_yellow = get_node("/root/Scene/HUD/YellowIndicator")
+
 func get_cones():
 	var img = get_viewport().get_texture().get_data()  # image is flipped in y
 	img.shrink_x2()
@@ -18,27 +21,32 @@ func get_cones():
 	img.shrink_x2()
 	img.lock()
 	var output = ""
-	var blue_pixel = -1
-	var yellow_pixel = -1
+	var blue_pixel = Vector2(-1, -1)
+	var yellow_pixel = Vector2(-1, -1)
 	for y in range(0, img.get_height() / 2):
 		for x in range(img.get_width()):
 			var pixel = img.get_pixel(x, y).to_html(false)
 			if pixel == blue_pixel_ref:
-				blue_pixel = x
+				blue_pixel = Vector2(x, y)
 			elif pixel == yellow_pixel_ref:
-				yellow_pixel = x
-			if blue_pixel != -1 and yellow_pixel != -1:
-				return Vector3(blue_pixel, yellow_pixel, img.get_width())
-	return Vector3(blue_pixel, yellow_pixel, img.get_width())
+				yellow_pixel = Vector2(x, y)
+			if blue_pixel.x != -1 and yellow_pixel.x != -1:
+				return [blue_pixel, yellow_pixel, img.get_width(), img.get_height()]
+	return [blue_pixel, yellow_pixel, img.get_width(), img.get_height()]
 
-func controller(blue_pixel, yellow_pixel, width):
-	var error = width / 2 - (blue_pixel + yellow_pixel) / 2
+func controller(blue_pixel, yellow_pixel, width, height):
+	var error = width / 2 - (blue_pixel.x + yellow_pixel.x) / 2
 	return error * k_p
 
 func _physics_process(delta):
 	## Check for cones
-	var cones = get_cones()
-	var input = controller(cones.x, cones.y, cones.z)
+	var pixels = get_cones()
+	var input = controller(pixels[0], pixels[1], pixels[2], pixels[3])
+	
+	## Update HUD
+	node_hud_blue.set_position(Vector2(0, pixels[3] / 2) * 16 + Vector2(pixels[0].x, -pixels[0].y) * 8)
+	node_hud_yellow.set_position(Vector2(0, pixels[3] / 2) * 16 + Vector2(pixels[1].x, -pixels[1].y) * 8)
+	
 
 	## Local vectors
 	var forward = get_transform().basis.x
@@ -55,4 +63,3 @@ func _physics_process(delta):
 	var v = get_linear_velocity()
 	apply_central_impulse(-left * v.project(left).length() * delta * 60 * sign(v.dot(left)))
 	apply_central_impulse(-linear_velocity * linear_friction)
-	print(linear_velocity.length())
