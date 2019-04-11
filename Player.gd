1b4597f765580abd03665ca0ca81a9d83109b5b1
Martin Gulliksson <martin@gullik.cc>
2019-04-11 20:53:28 +0200

    can dectect cones. slow though

--- Player.gd
+++ Player.gd
@@ -3,8 +3,29 @@ extends RigidBody
 const turn_torque = 80
 const acc_force = 120
 
-func _ready():
-	pass
+const blue_pixel_ref = "036eaf"
+const yellow_pixel_ref = "85982d"
+
+func get_cones():
+	var img = get_viewport().get_texture().get_data()
+	img.flip_y()
+	img.shrink_x2()
+	img.shrink_x2()
+	img.shrink_x2()
+	img.lock()
+	var output = ""
+	var blue_pixel = -1
+	var yellow_pixel = -1
+	for y in range(img.get_height(), img.get_height() / 2, -1):
+		for x in range(img.get_width(), 0, -1):
+			var pixel = img.get_pixel(x, y).to_html(false)
+			if pixel == blue_pixel_ref:
+				blue_pixel = x
+			elif pixel == yellow_pixel_ref:
+				yellow_pixel = x
+			if blue_pixel != -1 and yellow_pixel != -1:
+				return Vector2(blue_pixel, yellow_pixel)
+	return Vector2(blue_pixel, yellow_pixel)
 
 func _physics_process(delta):
 	## Local vectors
@@ -19,4 +40,8 @@ func _physics_process(delta):
 	## Sideways forces
 	var v = get_linear_velocity()
 	apply_central_impulse(-left * v.project(left).length() * delta * 60 * sign(v.dot(left)))
-	print(v.project(left))
+	
+	var cones = get_cones()
+	var blue_cone = cones.x
+	var yellow_cone = cones.y
+	print(str(blue_cone) + ", " + str(yellow_cone))
