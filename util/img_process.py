73308f86108a4c4f7c9ed4e0c63b2cdb69f31f19
Martin Gulliksson <martin@gullik.cc>
2019-04-22 14:26:15 +0200

    somewhat working curve fitting

--- util/img_process.py
+++ util/img_process.py
@@ -11,18 +11,33 @@ def main():
     image_path = "2019-04-22-124016_2560x1440_scrot.png"
     image = np.array(imageio.imread(image_path))
     image_blue_cones = np.zeros((np.shape(image)[0], np.shape(image)[1]))
+    image_blue_cones_reduced = []
     image_yellow_cones = np.zeros((np.shape(image)[0], np.shape(image)[1]))
+    image_yellow_cones_reduced = []
     for i in range(len(image)):
         for j in range(len(image[i])):
             if np.equal(image[i, j], color_blue_cone).all():
                 image_blue_cones[i, j] = 1
+                image_blue_cones_reduced.append((i,j))
             if np.equal(image[i, j], color_yellow_cone).all():
                 image_yellow_cones[i, j] = 1
-    
+                image_yellow_cones_reduced.append((i, j))
+
+    x = np.linspace(0, np.shape(image)[1])
     plt.figure()
     plt.imshow(image_blue_cones, cmap='Blues', interpolation='nearest')
+    image_blue_cones_reduced = np.array(image_blue_cones_reduced).transpose()
+    fit_blue = np.polyfit(image_blue_cones_reduced[0], image_blue_cones_reduced[1], 1)
+    print(f"Fit blue = {fit_blue}")
+    plt.plot(x, fit_blue[0] * x + fit_blue[1])
+    plt.ylim(np.shape(image)[0], 0)
     plt.figure()
     plt.imshow(image_yellow_cones, cmap='Oranges', interpolation='nearest')
+    image_yellow_cones_reduced = np.array(image_yellow_cones_reduced).transpose()
+    fit_yellow = np.polyfit(image_yellow_cones_reduced[0], image_yellow_cones_reduced[1], 1)
+    print(f"Fit yellow = {fit_yellow}")
+    plt.plot(x, fit_yellow[0] * x + fit_yellow[1], 'r')
+    plt.ylim(np.shape(image)[0], 0)
     plt.show()
 
 
