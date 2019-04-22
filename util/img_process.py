6082b40119820009e9a36bb841cc1c4892355b58
Martin Gulliksson <martin@gullik.cc>
2019-04-22 14:58:08 +0200

    cleaned up a bit

--- util/img_process.py
+++ util/img_process.py
@@ -8,7 +8,6 @@ color_blue_cone = (13, 71, 161)
 color_yellow_cone = (255, 214, 0)
 
 def main():
-    image_path = "2019-04-22-124016_2560x1440_scrot.png"
     image = np.array(imageio.imread(image_path))
     image_blue_cones = np.zeros((np.shape(image)[0], np.shape(image)[1]))
     image_blue_cones_reduced = []
@@ -19,7 +18,7 @@ def main():
             if np.equal(image[i, j], color_blue_cone).all():
                 image_blue_cones[i, j] = 1
                 image_blue_cones_reduced.append((i,j))
-            if np.equal(image[i, j], color_yellow_cone).all():
+            elif np.equal(image[i, j], color_yellow_cone).all():
                 image_yellow_cones[i, j] = 1
                 image_yellow_cones_reduced.append((i, j))
 
